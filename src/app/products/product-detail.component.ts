043afa51c8f377863a5da1cb92adbb80c6931395
Savvas Filippidis <sfilippidis@bossinfo.ch>
2021-06-14 10:05:12 +0300

    updates

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+
+@Component({
+    templateUrl: './product-detail.component.html',
+    styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+    pageTitle: string = 'Product detail';
+
+    constructor(private route: ActivatedRoute,
+                private router: Router){
+
+    }
+
+    ngOnInit(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        this.pageTitle += `: ${id}`;
+    }
+}
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product-service";
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List!';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
 
     private _listFilter: string = '';
     get listFilter(): string {
@@ -24,26 +26,35 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[] = [];
-    products: IProduct[] = []; 
+    products: IProduct[] = [];
 
-    constructor(private productService: ProductService){}
+    constructor(private productService: ProductService) { }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products
+                this.filteredProducts = products
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) => 
+        return this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().includes(filterBy));
     }
 
-    onRatingClicked(message: string): void{
+    onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
     }
 }
